0efda9a4abb3158da9d023bfba5276308bf12aa1
Kash Somani <kash@kashsomani.com>
2021-08-15 11:24:42 +0530

    responsive nav with better sign out

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -8,12 +8,12 @@
 </script>
 
 <div class="fixed top-0 w-full h-1  text-center">
-    <div class="p-4 grid grid-cols-5 place-items-center embossed m-4">
-        <div>
-            <button class="depressed p-4 font-bold" on:click={signOutCheck}>Sign Out</button>
+    <div class="p-4 grid grid-cols-11 place-items-center embossed m-4">
+        <div class="col-span-3 grid grid-cols-1 place-items-center">
+            <button class="depressed p-4 font-bold text-sm md:text-xl" on:click={signOutCheck}>Sign Out</button>
         </div>
-        <div class="col-span-1"></div>
-        <div>LA Admin Console</div>
         <div class="col-span-2"></div>
+        <div>LA Admin Console</div>
+        <div class="col-span-5"></div>
     </div>
 </div>
